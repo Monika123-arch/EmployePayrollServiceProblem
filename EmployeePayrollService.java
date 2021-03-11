ec783dc2844d72be74af06c463f6e815b1044671
Monika <monikadkale98@gmail.com>
2021-03-11 13:33:05 +0530

    print employe pay roll

--- EmployeePayrollService.java
+++ EmployeePayrollService.java
@@ -3,7 +3,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+	public enum IOService {CONSOLE_ID, FILE_IO, DB_IO, REST_IO}
 	private List<EmployeePayrollData> employeePayrollList;
+	
+	public EmployeePayrollService() {}
 	
 	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
 		this.employeePayrollList = employeePayrollList;
@@ -14,7 +17,7 @@ public class EmployeePayrollService {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
 		Scanner consoleInputReader = new Scanner(System.in);
 		employeePayrollService.readEmployeePayrollData(consoleInputReader);
-		employeePayrollService.writeEmployeePayrollData();
+		employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_ID);
 	}
 
 	private void readEmployeePayrollData(Scanner consoleInputReader) {
@@ -27,8 +30,25 @@ public class EmployeePayrollService {
 		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
 	}
 	
-	private void writeEmployeePayrollData() {
-		System.out.println("Writing Employee Payroll Data to Console\n" + employeePayrollList);
+	public void writeEmployeePayrollData(IOService ioService) {
+		if(ioService.equals(EmployeePayrollService.IOService.CONSOLE_ID)) {
+			System.out.println("Writing Employee Payroll Data to Console\n" + employeePayrollList);
+		}
+		else if(ioService.equals(EmployeePayrollService.IOService.FILE_IO)) {
+			new EmployeePayRollFileIOService().writeData(employeePayrollList);
+		}
+	}
+	
+	public void printData(IOService ioService) {
+		if(ioService.equals(EmployeePayrollService.IOService.FILE_IO)) {
+			new EmployeePayRollFileIOService().printData();
+		}
 	}
 
+	public long countEntries(IOService ioService) {
+		if(ioService.equals(EmployeePayrollService.IOService.FILE_IO)) {
+			return new EmployeePayRollFileIOService().countEntries();
+		}	
+		return 0;
+	}
 }
