d652afb552222d5f1dc057a13ccbb0826c9ee60a
Monika <monikadkale98@gmail.com>
2021-03-11 13:42:35 +0530

    read file IO

---
+++ EmployeePayrollServiceTest.java
@@ -0,0 +1,26 @@
+package com.employeepayroll;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class EmployeePayrollServiceTest {
+
+public class EmployeePayRollServiceTest {
+
+    @Test
+    public void given3EmployeeWhenWrittenToFile_ShouldReturnEmployeeEnteries(){
+        EmployeePayRollData[] arrayOfEmps ={
+            new EmployeePayRollData(1,"monika",100000),
+            new EmployeePayRollData(2,"komal",100000),
+            new EmployeePayRollData(3,"ram",100000),
+        };
+        EmployeePayRollService employeePayRollService;
+        employeePayRollService = new EmployeePayRollService(Arrays.asList(arrayOfEmps));
+
+        employeePayRollService.writeData(EmployeePayRollService.IOService.FILE_IO);
+
+        employeePayRollService.writeData();
+
+    }
+}
